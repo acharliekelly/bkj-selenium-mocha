9d3627259f44de5565d5e209b13c5ee08a9727a5
Charlie Kelly <charliekelly31415@gmail.com>
2024-10-01 16:46:55 -0400

    copy from LambdaTest

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bkj-selenium-mocha
 BKJ WebCare Automation in Selenium Mocha 
+
+loosely based on [LambdaTest mocha selenium sample](https://github.com/LambdaTest/mocha-selenium-sample)
---
+++ specs/parallel_test.js
@@ -0,0 +1,61 @@
+var assert = require("assert"),
+  webdriver = require("selenium-webdriver"),
+  conf_file = process.argv[3] || "conf/single.conf.js";
+
+var capabilities = require("../" + conf_file).capabilities;
+
+var buildDriver = function(caps) {
+  return new webdriver.Builder()
+    .usingServer(
+      "http://" +
+        LT_USERNAME +
+        ":" +
+        LT_ACCESS_KEY +
+        "@hub.lambdatest.com/wd/hub"
+    )
+    .withCapabilities(caps)
+    .build();
+};
+
+capabilities.forEach(function(caps) {
+ 
+  describe("Mocha Todo Test " + caps.browserName, function() {
+    var driver;
+    this.timeout(0);
+
+    beforeEach(function(done) {
+      caps.name = this.currentTest.title;
+      driver = buildDriver(caps);
+      done();
+    });
+
+    it("can find search results" + caps.browserName, function(done) {
+      driver.get("https://lambdatest.github.io/sample-todo-app/").then(function() {
+          driver.findElement(webdriver.By.name('li1')).click().then(function(){
+            console.log("Successfully clicked first list item.");
+        });
+
+        driver.findElement(webdriver.By.name('li2')).click().then(function(){
+            console.log("Successfully clicked second list item.");
+          });
+
+        driver.findElement(webdriver.By.id('sampletodotext')).sendKeys('Complete Lambdatest Tutorial\n').then(function(){
+            driver.findElement(webdriver.By.id('addbutton')).click().then(function(){
+                console.log("Successfully added a new task.");
+            })
+        });
+      });
+    });
+
+    afterEach(function(done) {
+      if (this.currentTest.isPassed()) {
+        driver.executeScript("lambda-status=passed");
+      } else {
+        driver.executeScript("lambda-status=failed");
+      }
+      driver.quit().then(function() {
+        done();
+      });
+    });
+  });
+});
